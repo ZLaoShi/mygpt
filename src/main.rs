91189234260ca926c9bb5f99bed586d202e7a8f8
mxwj <z2116344865@gmail.com>
2025-08-11 23:56:51 +0800

    feat: add genrate

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use burn::backend::Wgpu;
 use burn::nn::loss::CrossEntropyLoss;
 use burn::nn::{Embedding, EmbeddingConfig};
 use burn::prelude::Backend;
+use burn::tensor::activation::softmax;
 use burn::tensor::{Int, Tensor, TensorData};
+use rand::prelude::*;
 
 pub struct Tokenizer{
     stoi:HashMap<char, i32>,
@@ -133,6 +135,29 @@ impl <B: Backend> BigramModel<B> {
     }
 }
 
+pub fn genrate<B: Backend>(model:&BigramModel<B>, prompt:Vec<i32>, max_new_tokens: usize, device:&B::Device) -> Vec<i32> {
+    let mut tokens = prompt;
+    let mut rng = rand::rng();
+
+    for i in tokens.len()..tokens.len() + max_new_tokens {
+        let x = TensorData::new(tokens.clone(), [1, i]);
+        let x = Tensor::<B, 2, Int>::from_data(x, device);
+
+        let logits = model.forward(x);
+        let logits = logits.slice([0..1, i - 1..i]);
+
+        let props = softmax(logits, 2);
+        let props = props.to_data().to_vec::<f32>().unwrap();
+
+        let distr = rand::distr::weighted::WeightedIndex::new(&props).unwrap();
+        let next_token = distr.sample(&mut rng) as i32;
+
+        tokens.push(next_token);
+    }
+ 
+    tokens
+}
+
 fn main() {
     type MyBackend = Wgpu;
     let device = WgpuDevice::default();
@@ -158,5 +183,8 @@ fn main() {
     println!("{logits}");
 
     let loss = model.loss(x, y, &device);
-    println!("{loss}")
+    println!("{loss}");
+
+    let new_tokens = genrate(&model, tokenizer.encode("A"), 100, &device);
+    println!("{}", tokenizer.decode(&new_tokens));
 }
